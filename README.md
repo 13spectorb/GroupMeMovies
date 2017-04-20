42db2066ea22268d311bc97613d47a37d2ddb05a
Ben Spector <benjamin_spector@brown.edu>
2017-04-20 11:29:57 -0400

    first draft of bot without movie grabbing, will respond hi to 'movies'

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GroupMeMovies
 GroupMe Movie Bot! - Leverage
+
+<!-- 
+Process:
+- GroupMe HTTP POST on each message sent
+- parse POST body for "movie"
+- Query movie database
+- format response
+- reply with HTTP POST to the group
+ -->
--- moviebot.js
+++ moviebot.js
@@ -1,45 +1,67 @@
 /* Node.js */
-var http = require("http");
 
-http.createServer(function (request, response) {
-
-   
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
-
-http.createServer(function(request, response) {
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
-  request.on('error', function(err) {
-    console.error(err);
-  }).on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-    // At this point, we have the headers, method, url and body, and can now
-    // do whatever we need to in order to respond to this request.
-  });
-
-  // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-  
-}).listen(8080); // Activates this server, listening on port 8080.
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
-
+/*
 groupme_token="f80cc46006ca01352bfd07722314ca56";
 group_id="30555241"
 group_name="MovieChat";
-bot_id="76651f6a40966c5143a76c44e3"
+bot_id="dd48f9623940e77715a0874f5a"
+*/
 
-var xhr = new XMLHttpRequest();
+var https = require("https");
+
+var bot_id = "dd48f9623940e77715a0874f5a"
+
+function respond() {
+	var request = JSON.parse(this.request.body[0]),
+		regex = new RegExp('movie', 'MOVIE', 'Movie');
+
+	if(request.text && regex.test(request.text)) {
+		this.response.writeHead(200);
+		postMovies(getMovies());
+		this.response.end();
+	} else {
+		this.response.writeHead(200);
+		this.response.end();
+	}
+}
+
+function getMovies() {
+	return "hi"
+}
+
+function postMovies(movies) {
+	var options, body, request;
+
+	options = {
+		hostname: 'https://groupme-movie-bot.herokuapp.com/',
+		path: '/v3/bots/post',
+		method: 'POST'
+	};
+
+	body = {
+		"bot_id" : 'dd48f9623940e77715a0874f5a',
+		"text" : movies
+	}
+
+	request = HTTPS.request(options, function(response) {
+		if(response.statusCode == 202) {
+       	 //neat
+      	} else {
+        	console.log('rejecting bad status code ' + response.statusCode);
+      	}
+	});
+
+	request.on('error', function(err) {
+    	console.log('error:' + JSON.stringify(err));
+  	});
+  	request.on('timeout', function(err) {
+	    console.log('timeout: ' + JSON.stringify(err));
+  	});
+	request.end(JSON.stringify(body));
+}
+
+exports.respond = respond;
+// var xhr = new XMLHttpRequest();
 
 
 
